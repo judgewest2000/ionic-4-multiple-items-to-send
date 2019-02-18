6c581b4a04ba14b5669ccec6f2b1de096296fdb4
James West <james.west@westdarley.co.uk>
2019-02-18 16:11:23 +0000

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+
+interface ItemDefinition {
+  name: string;
+  description: string;
+  checked: boolean;
+  count: number;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+
+  items = <ItemDefinition[]>[
+    {
+      name: 'First',
+      description: 'This is the first item',
+      checked: false,
+      count: 0
+    },
+    {
+      name: 'Second',
+      description: 'This is the second item',
+      checked: false,
+      count: 0
+    }
+  ];
+
+  itemsToExport = <ItemDefinition[]>[];
+
+  refreshPayload() {
+    this.itemsToExport = this.items.filter(x => x.checked);
+  }
 
 }
